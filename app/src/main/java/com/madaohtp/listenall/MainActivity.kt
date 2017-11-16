a0554256529ad0afcf253a266a99b9689e273b20
hetianpeng <t.hcc@foxmail.com>
2017-11-16 17:39:49 +0800

    。。。

--- app/src/main/java/com/madaohtp/listenall/MainActivity.kt
+++ app/src/main/java/com/madaohtp/listenall/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.madaohtp.listenall
 
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mediaPlayer = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaPlayer.setAudioAttributes(
+                    AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                            .build()
+            )
+        } else {
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        }
+
+//        mediaPlayer.setDataSource()
+
+        mediaPlayer.setOnPreparedListener(object : MediaPlayer.OnPreparedListener {
+            override fun onPrepared(mp: MediaPlayer?) {
+                toast("准备成功")
+                mediaPlayer.start()
+            }
+        })
+
+        mediaPlayer.prepareAsync()
     }
 }
