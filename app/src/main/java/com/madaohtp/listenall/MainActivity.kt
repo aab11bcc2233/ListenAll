a1d317f78e10428b1e15c9b5862b3c6d96fcc69e
hetianpeng <t.hcc@foxmail.com>
2017-11-17 16:40:43 +0800

    。。。

--- app/src/main/java/com/madaohtp/listenall/MainActivity.kt
+++ app/src/main/java/com/madaohtp/listenall/MainActivity.kt
@@ -4,37 +4,54 @@ import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mediaPlayer = MediaPlayer()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mediaPlayer.setAudioAttributes(
-                    AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                            .setLegacyStreamType(AudioManager.STREAM_MUSIC)
-                            .build()
-            )
-        } else {
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        mediaPlayer = MediaPlayer()
+
+        if (mediaPlayer != null) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mediaPlayer?.setAudioAttributes(
+                        AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                                .build()
+                )
+            } else {
+                mediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+            }
+
+            mediaPlayer?.setDataSource("http://192.168.2.120:8080/xxx.mp3")
+
+            mediaPlayer?.setOnPreparedListener(object : MediaPlayer.OnPreparedListener {
+                override fun onPrepared(mp: MediaPlayer?) {
+                    toast("准备成功")
+                    mediaPlayer?.duration
+                    mediaPlayer?.start()
+                }
+            })
+
+            mediaPlayer?.prepareAsync()
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mediaPlayer != null) {
+            mediaPlayer?.stop()
+            mediaPlayer?.release()
         }
 
-//        mediaPlayer.setDataSource()
-
-        mediaPlayer.setOnPreparedListener(object : MediaPlayer.OnPreparedListener {
-            override fun onPrepared(mp: MediaPlayer?) {
-                toast("准备成功")
-                mediaPlayer.start()
-            }
-        })
-
-        mediaPlayer.prepareAsync()
     }
 }
